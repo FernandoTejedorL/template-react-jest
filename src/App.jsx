d3e507d0807eedf9a679e2ef5143214a50f94342
Fernando Tejedor <fernandotejedorlopez@gmail.com>
2025-03-07 14:31:04 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Link } from 'react-router-dom';
+import Router from './router/Router';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyles />
+
+			<BrowserRouter>
+				<nav>
+					<ul>
+						<Link to='/'>Home</Link>
+					</ul>
+					<ul>
+						<Link to='/about'>About</Link>
+					</ul>
+				</nav>
+				<button>One</button>
+				<button>Two</button>
+				<button>Three</button>
+				<Router />
+			</BrowserRouter>
+		</>
+	);
+};
+
+export default App;
